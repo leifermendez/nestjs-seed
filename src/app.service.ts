fe39e5898f35bd6cef1cd1ede958bf04ea085c63
Leifer Mendez <leifer33@gmail.com>
2022-04-19 17:02:11 +0200

    Init

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getList(): string[] {
+    return ['item','item','item'];
+  }
+}
